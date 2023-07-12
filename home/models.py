a43418dd1e70a229580a07ac505574ff3d3752e2
Sakol Bun <sakol.bun01@student.csulb.edu>
2023-07-11 19:10:53 -0700

    Added customer model

--- home/models.py
+++ home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
 # Create your models here.
 class ProductCategories(models.Model):
     name = models.CharField(max_length=48)
@@ -21,8 +21,24 @@ class Product(models.Model):
     created_at = models.DateTimeField("Created At", auto_now_add=True)
     updated_at = models.DateTimeField("Updated At", auto_now=True)
     class Meta:
-        db_table="Products"
-        
+        db_table = "Products"
+
     def __str__(self):
         return self.name
-    
+
+class Customer(models.Model):
+    phone_regex = RegexValidator(
+        regex = r'^\+?1?\d{9, 15}$',
+        message = "Phone number must be in the correct U.S number format"
+    )
+    first_name = models.CharField("First Name",max_length=128)
+    last_name = models.CharField("Last Name", max_length=64)
+    phone_number = models.CharField(validators=[phone_regex], max_length=17)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = "Customers"
+
+    def __str___(self):
+        return self.firstname + " " + self.last_name
+
