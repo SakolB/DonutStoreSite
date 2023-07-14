2364f00e1c6cfe4b81db235bebd84d47b06d9368
Sakol Bun <sakol.bun01@student.csulb.edu>
2023-07-13 18:48:44 -0700

    created an edit view and template

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "home"
 urlpatterns = [
     path("", views.home, name="index"),
+    path("edit/", views.edit, name="edit"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html', {})
 
 def cart(request):
     return
+
+@login_required(login_url="/admin")
+def edit(request):
+    return render(request, "home/edit.html", {})
