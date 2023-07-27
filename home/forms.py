d5351d76904f6157f3c3e3d0a2f5e5cc0137be2a
Sakol Bun <112042178+SakolB@users.noreply.github.com>
2023-07-26 18:24:09 -0700

    Create Profile form

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Product
+from .models import Product, Profile
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price', 'photo', 'category']
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'profile_pic']
